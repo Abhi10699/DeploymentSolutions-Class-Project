4345fbb8325f43dae3d2990011385f1eff4dde9c
Abhi10699 <patelabhi10699@gmail.com>
2024-02-02 13:25:54 -0500

    added for testing workflow

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ def add(a, b):
     raise Exception("Need A or B for addition")
   return a+b
 
+def test_function():
+  assert add(1,2) == 3
 
-a = int(input("Enter Number 1: "))
-b = int(input("Enter Number 2: "))
+if __name__ == "__main__":
+  a = int(input("Enter Number 1: "))
+  b = int(input("Enter Number 2: "))
 
-sum = add()
-print(f"Sum of {a} and {b} is {sum}")
+  sum = add()
+  print(f"Sum of {a} and {b} is {sum}")
