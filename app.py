8f372870ba9241769e54440ead0f9070e79420ba
Abhi10699 <patelabhi10699@gmail.com>
2024-02-02 12:52:13 -0500

    added error handling

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 def add(a, b):
+  if not a or not b:
+    raise Exception("Need A or B for addition")
   return a+b
 
 
 a = int(input("Enter Number 1: "))
 b = int(input("Enter Number 2: "))
 
-sum = a+b
+sum = add()
 print(f"Sum of {a} and {b} is {sum}")
